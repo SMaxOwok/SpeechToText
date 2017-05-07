22b45675d042bdea089e62d16404e96f055f87c5
Max Ono <max@castironcoding.com>
2017-05-07 17:16:48 -0400

    Display spoken text

--- app/src/main/java/com/example/mono/speechtotext/Conversation.java
+++ app/src/main/java/com/example/mono/speechtotext/Conversation.java
@@ -1,24 +1,79 @@
 package com.example.mono.speechtotext;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.speech.RecognizerIntent;
-import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Conversation extends AppCompatActivity implements OnClickListener {
+import java.util.ArrayList;
+import java.util.Locale;
+
+
+public class Conversation extends AppCompatActivity {
+
+    private TextView comment;
+    private Button button;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation);
+
+        comment = (TextView) findViewById(R.id.comment);
+        button = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+            }
+        });
+
     }
 
-    @Override
-    public void onView(View v) {
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    /**
+     * Receiving speech input
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    comment.setText(result.get(0));
+                    comment.setVisibility(View.VISIBLE);
+                }
+                break;
+            }
+
+        }
     }
 }
